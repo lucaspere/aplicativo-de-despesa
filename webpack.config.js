22a9b36b42e361421a7b617872afc6a8d21427e6
soudogueto <lucasrupestre@hotmail.com>
2020-03-01 23:47:41 -0300

    configurando os css loader e babel loaders no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,21 @@ module.exports = {
       path: require('path').join(__dirname, 'public'),
       filename: 'bundle.js'
    },
+   mode: "development",
+   module: {
+      rules: [{
+         loaders: "babel-loader",
+         test: /\.jsx?$/,
+         exclude: /node_modules/
+      }, {
+         teste: /\.s?css$/,
+         use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+         ]
+      }]
+   },
    devtool: 'cheap-module-aval-source-map',
    devServer: {
       contentBase: path.join(__dirname, 'public'),
