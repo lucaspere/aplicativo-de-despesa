25cc381626fe6c1c8eeec73c0c7b1cc4b66ab628
soudogueto <lucasrupestre@hotmail.com>
2020-03-05 00:08:08 -0300

    adicionando opções de categorias de despesa

--- src/components/ExpenseDashboardPage.jsx
+++ src/components/ExpenseDashboardPage.jsx
@@ -16,6 +16,19 @@ const ExpenseDashboardPage = () => {
                type="text"
                value={expenseValue}
                onChange={(e) => setExpenseValue(e.target.value)} />
+            <label for="category">Categoria</label>
+            <select id="category">
+               <option value="restaurants">Restaurantes</option>
+               <option value="shopping">Compras</option>
+               <option value="habitation">Habitação</option>
+               <option value="transport">Transporte</option>
+               <option value="vehicle">Veiculo</option>
+               <option value="entertainment">Entreterimento</option>
+               <option value="comunication">Comunicação</option>
+               <option value="financial expenses">Despesas Financeiras</option>
+               <option value="investiment">Investimento</option>
+               <option value="income">Renda</option>
+            </select>
             <button type="submit">Adicionar</button>
          </form>
 
